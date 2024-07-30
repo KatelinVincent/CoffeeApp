c8d5fb6c9fc9edc1cae60802f72613e6076e45d1
Katelin Vincent <katelinannvincent@gmail.com>
2024-07-29 21:47:06 -0500

    feat : nav bar, existing profile page and new profile form

--- app/src/main/java/com/example/grabngocoffee/MainActivity.kt
+++ app/src/main/java/com/example/grabngocoffee/MainActivity.kt
@@ -37,15 +37,30 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.grabngocoffee.ui.theme.GrabnGoCoffeeTheme
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.TextDecoration
 
 data class MenuItem(
     val name: String,
     val image: Int = R.drawable.placeholder,
     val price: Double,
     val description: String
+)
+
+data class UserProfile(
+    val firstName: String = "",
+    val lastName: String = "",
+    val birthday: String = "",
+    val rewardsMember: Boolean = false
 )
 
 class MainActivity : ComponentActivity() {
@@ -70,9 +85,68 @@ fun MainScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.DarkGray)
+            .background(Color.DarkGray),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        TitleName(name = stringResource(R.string.grab_and_go_coffee))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                AsyncImage(
+                    model = R.drawable.coffee_icon,
+                    contentDescription = "Coffee Icon",
+                    modifier = Modifier
+                        .size(40.dp)
+                )
+
+                TitleName(name = stringResource(R.string.grab_and_go_coffee))
+            }
+        }
+        var menuActivePage by remember {
+            mutableStateOf(true)
+        }
+        var profileActivePage by remember {
+            mutableStateOf(false)
+        }
+        var historyActivePage by remember {
+            mutableStateOf(false)
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "menu",
+                color = if (menuActivePage) Color.White else Color.Unspecified,
+                textDecoration = if (menuActivePage) TextDecoration.Underline else TextDecoration.None,
+                modifier = Modifier.clickable {
+                    menuActivePage = true
+                    profileActivePage = false
+                    historyActivePage = false
+                })
+            Text(text = "past orders",
+                color = if (historyActivePage) Color.White else Color.Unspecified,
+                textDecoration = if (historyActivePage) TextDecoration.Underline else TextDecoration.None,
+                modifier = Modifier.clickable {
+                    historyActivePage = true
+                    menuActivePage = false
+                    profileActivePage = false
+                })
+            Text(text = "profile",
+                color = if (profileActivePage) Color.White else Color.Unspecified,
+                textDecoration = if (profileActivePage) TextDecoration.Underline else TextDecoration.None,
+                modifier = Modifier.clickable {
+                    profileActivePage = true
+                    menuActivePage = false
+                    historyActivePage = false
+                })
+        }
 
         errorMessage?.let {
             Text(
@@ -80,7 +154,155 @@ fun MainScreen() {
                 color = Color.Red,
             )
         } ?: run {
-            MenuContent(setErrorMessage)
+            when {
+                menuActivePage -> MenuContent(setErrorMessage)
+                profileActivePage -> profileContent(setErrorMessage)
+                historyActivePage -> historyContent(setErrorMessage)
+                else -> MenuContent(setErrorMessage)
+            }
+        }
+    }
+}
+
+@Composable
+fun historyContent(setErrorMessage: (String) -> Unit) {
+
+}
+
+@Composable
+fun profileContent(setErrorMessage: (String) -> Unit) {
+    var makeNewProfile by remember { mutableStateOf(false) }
+    var profiles by remember { mutableStateOf(listOf<UserProfile>()) }
+
+    Column(modifier = Modifier.padding(vertical = 8.dp)
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (makeNewProfile == true)
+            NewProfile(
+                onProfileCreated = { newProfile ->
+                    profiles = profiles + newProfile
+                    makeNewProfile = false
+                },
+                onCancel = { makeNewProfile = false }
+            )
+        else {
+            ExistingProfiles(profiles = profiles)
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { makeNewProfile = true },
+                modifier = Modifier.height(36.dp)
+            ) {
+                Text(text = "+ new profile")
+            }
+        }
+    }
+}
+
+@Composable
+fun NewProfile(
+    onProfileCreated: (UserProfile) -> Unit,
+    onCancel: () -> Unit
+) {
+
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
+    var birthday by remember { mutableStateOf("") }
+    var rewardsMember by remember { mutableStateOf(false) }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        TextField(value = firstName,
+            onValueChange = { firstName = it },
+            label = { Text("First Name") }
+        )
+        TextField(value = lastName,
+            onValueChange = { lastName = it },
+            label = { Text(text = "Last Name") })
+        TextField(value = birthday,
+            onValueChange = { birthday = it },
+            label = { Text(text = "birthday") })
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(vertical = 8.dp)
+        ) {
+            Checkbox(
+                checked = rewardsMember,
+                onCheckedChange = { rewardsMember = it },
+                colors = CheckboxDefaults.colors(
+                    checkedColor = MaterialTheme.colorScheme.primary,
+                    uncheckedColor = Color.White,
+                    checkmarkColor = Color.White
+                )
+            )
+            Text(
+                text = "RewardsMember",
+                color = Color.White
+            )
+        }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Button(
+                    onClick = { onCancel() },
+                ) {
+                    Text(text = "Cancel")
+                }
+                Button(
+                    onClick = {
+                        if (firstName.isNotBlank() && lastName.isNotBlank() && birthday.isNotBlank()) {
+                            val newProfile = UserProfile(firstName, lastName, birthday, rewardsMember)
+                            onProfileCreated(newProfile)
+                        }
+                    },
+                ) {
+                    Text(text = "Create Profile")
+                }
+            }
+    }
+}
+
+@Composable
+fun ExistingProfiles(profiles: List<UserProfile>) {
+    Column (modifier = Modifier.padding(16.dp)) {
+        Text(
+            "Existing Profiles",
+            style = MaterialTheme.typography.headlineSmall,
+            color = Color.White,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        if (profiles.isEmpty()) {
+            Text("No profiles created yet", color = Color.White)
+        } else {
+            profiles.forEach { profile ->
+                ProfileItem(profile)
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileItem(profile: UserProfile){
+    Surface(
+        modifier = Modifier.fillMaxWidth(),
+        color = Color.White,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("${profile.firstName} ${profile.lastName}", style = MaterialTheme.typography.titleMedium)
+            Text("Birthday: ${profile.birthday}", style = MaterialTheme.typography.bodyMedium)
+            Text(
+                "Rewards Member: ${if (profile.rewardsMember) "Yes" else "No"}",
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
@@ -171,7 +393,7 @@ fun MenuContent(setErrorMessage: (String) -> Unit) {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp, vertical =  6.dp),
+            .padding(horizontal = 16.dp, vertical = 6.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         categories.forEach { (category, items) ->
@@ -193,9 +415,6 @@ fun MenuContent(setErrorMessage: (String) -> Unit) {
 fun TitleName(name: String) {
     Text(
         text = name,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp),
         style = MaterialTheme.typography.displaySmall.copy(
             fontWeight = FontWeight.SemiBold
         ),
@@ -265,7 +484,7 @@ fun DisplayItem(item: MenuItem) {
             onClick = {},
             Modifier.height(36.dp)
 
-        ){
+        ) {
             Text("+ add to cart")
         }
     }
